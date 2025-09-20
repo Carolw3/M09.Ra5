59a62267202cf49b9cc177c4c45215120ab2db65
Carolw3 <carolina.melguizos3@gmail.com>
2025-09-20 17:05:01 +0200

    Rot13 acabats codifica i descodificafalta sincronitzar abecdaris

--- Rot13/src/Rot13.java
+++ Rot13/src/Rot13.java
@@ -1,52 +1,87 @@
+/* Este programa tiene por objetivo codificar un texto sumando 13 posiciones
+ respecto a la posicion de la letra en los abecedarios especificados */
+
 public class Rot13 {
-    static String minuscul = "aàáäbcdeèéëfghiìíïjklmnoòóöpqrstuùúüvwxyz";
-    static String majuscul = "AÀÁÄBCDEÈÉËFGHIÌÍÏJKLMNOÒÓÖPQRSTUÙÚÜVWXYZ";
+    static String minuscul = "aàáäbcçdeèéëfghiìíïjklmnñoòóöpqrstuùúüvwxyz";
+    static String majuscul = "AÀÁÄBCÇDEÈÉËFGHIÌÍÏJKLMNÑOÒÓÖPQRSTUÙÚÜVWXYZ";
 
     static char[] min=minuscul.toCharArray();
     static char[] maj=majuscul.toCharArray();
-
-    static String text = "Hola que tal estàs?";
-
     public static void main(String[] args) {
-       String textCo = Rot13.codifica(text);
-       System.out.println(textCo);
+        String[] textArray = {
+            "ABC",
+            "XYZ",
+            "Hola, Mr. calçot",
+            "Perdó, per tu què és?"
+        };
 
+        String[] textCArray ={
+            "IÏJ",
+            "FGH",
+            "Òwúi, Ùá. jiúkwb",
+            "Zmálx, zmá bc acñ nà?"
+        };
+
+        System.out.printf("Xifrat\n---------\n");
+
+        for(int i = 0 ; i < textArray.length ; i++ ){
+            String text = textArray[i];
+            String textACodi = Rot13.xifraRot13(text);
+
+            System.out.printf("%-30s \t => %-30s\n", text, textACodi);
+        }
+        System.out.printf("\nDesxifrat\n---------\n");
+
+        for(int i = 0 ; i < textArray.length ; i++ ){
+            String textC = textCArray[i];
+            String codiAText = Rot13.desxifraRot13(textC);
+
+            System.out.printf("%-30s \t => %-30s\n", textC, codiAText);
+        }
     }
 
-    public static String codifica(String text) {
+    public static String xifraRot13(String text) {
         String textCodificat = "";
         for (int i = 0; i < text.length(); i++) {
+            boolean trobat = false;
             char lletraText = text.charAt(i);
             for (int e = 0 ; e < min.length ; e++){
                 char lletraMin = min[e];
                 char lletraMaj = maj[e];
                 int index = 0;
                 if (lletraMaj == lletraText){
-                    if (e > (maj.length - 13)){
-                        index = (e + 13) % maj.length;
-                    }else{
-                        index = e + 13;
-                    }
-                    textCodificat += maj[index];
-                    break;
+                    textCodificat += maj[(e + 13) % maj.length];
+                    trobat = true;
                 }else if(lletraMin == lletraText){
-                    if (e > (min.length - 13)){
-                        index = (e + 13) % min.length;
-                    }else{
-                        index = e + 13;
-                    }
-                    textCodificat += min[index];
-                    break;
-                }else{
-                    textCodificat += lletraText;
-                    break;
+                    textCodificat +=  min[(e + 13) % min.length];
+                    trobat = true;
                 }
-
             }
-
+            if(!trobat){textCodificat += lletraText;}
         }
-
         return textCodificat;
+    }
+    public static String desxifraRot13(String codi) {
+        String textDescodificat = "";
+        for (int i = 0; i < codi.length(); i++) {
+            boolean trobat = false;
+            char lletra = codi.charAt(i);
+            for (int e = 0 ; e < min.length ; e++){
+                char lletraMin = min[e];
+                char lletraMaj = maj[e];
+                int index = 0;
+                if (lletraMaj == lletra){
+                    trobat = true;
+                    textDescodificat += maj[(e - 13 + maj.length ) % maj.length];
+                }else if(lletraMin == lletra){
+                    trobat = true;
+                    textDescodificat +=  min[(e - 13 + min.length) % min.length];
+                }
+            }
+            if(!trobat){textDescodificat += lletra;}
+        }
+        return textDescodificat;
+
     }
 
     
